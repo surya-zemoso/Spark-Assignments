dc5ff670c0f7f1b3b6b72820c821f95998e3c005
Suryakant <suryakant.kumar@zemosolabs.com>
2017-11-01 17:55:35 +0530

    Spark Assignments

---
+++ GDP/GDP.py
@@ -0,0 +1,34 @@
+from pyspark import SparkContext, SparkConf
+
+conf = SparkConf().setAppName('GDP Analysis').setMaster('local')
+sc = SparkContext()
+sqlContext = SQLContext(sc)
+
+
+# Reading gdp.csv file and create it a data frame
+df = spark.read.csv('gdp.csv',mode="DROPMALFORMED",inferSchema=True,header=True)
+#df.show()
+
+#Renaming the column of data frame
+df = df.withColumnRenamed("Country Name", "Country_Name")
+df = df.withColumnRenamed("Country Code", "Country_Code")
+df.createOrReplaceTempView("GDP_temp")
+
+#Creating a new Dataframe sqlDf which store GDP_growth_Rate
+sqlDF = spark.sql("SELECT d1.Country_Name, d1.Country_Code, d1.Year, d1.Value, (d2.Value - d1.Value)*100/d1.Value as GDP_growth_Rate FROM GDP_temp d1, GDP_temp d2 WHERE d1.Country_Code = d2.Country_Code AND d2.Year = d1.Year + 1")
+#sqlDF.show()
+sqlDF.createOrReplaceTempView("GDP_Rate")
+
+
+# Creating a dataframe which contain Year & maximum GdP value of that year 
+sqlDF1 = spark.sql("SELECT d1.Year,max(d1.GDP_growth_Rate) AS GDP_growth_Rate FROM GDP_Rate d1 GROUP BY d1.Year ORDER BY d1.Year")
+sqlDF1.createOrReplaceTempView("GDP_Max_Rate")
+#sqlDF1.show()
+
+#Creating a dataframe which contain year(having maximum GDP among all Country), Country_Name & the Value of GDP
+sqlDF2 = spark.sql("SELECT d1.Year, d1.Country_Name, d1.Value FROM GDP_Rate d1, GDP_Max_Rate d2 WHERE d1.Year = d2.Year AND d1.GDP_growth_Rate = d2.GDP_growth_Rate ORDER BY d1.Year")
+#sqlDF2.show()
+
+#Renaming Country_Name column to Country Name
+sqlDF2 = sqlDF2.withColumnRenamed("Country_Name", "Country Name")
+#sqlDF2.show()
---
+++ Twitter/twitter.py
@@ -0,0 +1,28 @@
+
+from pyspark import SparkContext, SparkConf
+
+conf = SparkConf().setAppName('Twitter Analysis').setMaster('local')
+sc = SparkContext(conf=conf)
+
+
+# Read file from desktop & create RDD
+allTweet = sc.textFile("/home/zemoso/Desktop/tweet.txt")
+
+#split the twitter file by " " and create single list RDD.
+splitTwit = allTweet.flatMap(lambda x: x.split(" "))
+ 
+# extracting # tag twit from splitTwit list
+tagTwit = splitTwit.filter(lambda x: x.startswith('#'))
+
+#create tuple of  #tag list & use reduceByKey to collect & count all tuple by same key
+tupleTwit = tagTwit.map(lambda t: (t,1)).reduceByKey(lambda x,y:x+y)
+
+#sorting the tuple by value
+sortedTwit = tupleTwit.sortBy(lambda a:-a[1])
+
+#extracting top 100 twit 
+top100 = sortedTwit.take(100)
+
+#printing top100 twit
+for i in top100:
+	print(i)
